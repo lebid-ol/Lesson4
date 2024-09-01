138fbb5ef2217312ab1448dab8cb4458af151cd3
lebid-ol <swan.programmer@gmail.com>
2024-09-01 13:23:18 +0300

    New_Branch

--- Lesson4_HomeTask/Program.cs
+++ Lesson4_HomeTask/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Lesson4_HomeTask
+﻿using System.Reflection.Metadata;
+using System.Text;
+
+namespace Lesson4_HomeTask
 {
     internal class Program
     {
@@ -11,19 +14,19 @@
             double d = 2.3;
 
             //символьный тип данных
-            char c = 'C';
+            char K = 'C';
 
             /* тип данных для хранения 
              * логических значений
              */
-            bool b = true;
+            bool T = true;
 
             /* хранений строчных
              * данных */
             string s1 = "Домашка";
 
             //строка форматирования: вывод переменных значений в строке
-            Console.WriteLine($"{i1}; {d}; {c}; {b}; {s1}");
+            Console.WriteLine($"{i1}; {d}; {K}; {T}; {s1}");
 
             int i2 = 4;
             int summa = i1 + i2;
@@ -39,6 +42,48 @@
 
             string s2 = " от 18 августа 2024 года";
             Console.WriteLine(s1 + s2);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            // Устанавливаем кодировку консоли на UTF-8
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("Домашка на 01.09.2024");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            int a,b,c;
+            a = 5;
+            b = 10;
+            c = 15;
+            var sum = a + b + c;
+            Console.WriteLine("Сумма трех чисел = " + sum);
+            var raznica = c - a;
+            Console.WriteLine("Разница между с и а = " + raznica); ;
+            var proizved = a * b;
+            Console.WriteLine($"Произведение а и b = {proizved}");
+            double delenie = (double)c / b;
+            Console.WriteLine($"Деление с на b = {delenie}");
+
+            double temperature = 36.6;
+            string temperaturetostring = temperature.ToString();
+            Console.WriteLine(temperaturetostring);
+
+            string numberstring = "123";
+            int number = int.Parse(numberstring);
+            Console.WriteLine(number);
+
+            string abcstring = "abc";
+            int abcnumber;
+            bool success = int.TryParse(abcstring, out abcnumber);
+            Console.WriteLine(success);
+
+
+
+
+
         }
     }
 }
